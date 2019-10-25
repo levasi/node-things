3eca9d8972db193a98e3cf88aa912f32344b34b1
Bogdan Vasile <evasile@hogash.com>
2019-10-25 18:54:46 +0300

    Update .gitignore

--- app.js
+++ app.js
@@ -15,23 +15,24 @@ app.use(upload()); // configure middleware
 
 console.log("Server Started at port 80");
 
-app.get('/',function(req,res){
-  res.sendFile(__dirname+'/index.html');
+app.get('/', function (req, res) {
+  res.sendFile(__dirname + '/index.html');
 })
-app.post('/upload',function(req,res){
+
+app.post('/upload', function (req, res) {
   console.log(req.files);
-  if(req.files.upfile){
+  if (req.files.upfile) {
     var file = req.files.upfile,
       name = file.name,
       type = file.mimetype;
     var uploadpath = __dirname + '/uploads/' + name;
-    file.mv(uploadpath,function(err){
-      if(err){
-        console.log("File Upload Failed",name,err);
+    file.mv(uploadpath, function (err) {
+      if (err) {
+        console.log("File Upload Failed", name, err);
         res.send("Error Occured!")
       }
       else {
-        console.log("File Uploaded",name);
+        console.log("File Uploaded", name);
         res.send('Done! Uploading files')
       }
     });
